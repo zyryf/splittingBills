a7a7e4657de0cf7636d3d64bd1971f0f086e7c17
szymon <szymon.borkowski@gmail.com>
2020-05-09 21:55:01 +0200

    add grabing user info functionality

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
-    <p>Welcome !</p>
+    <h2>Welcome {{ user.name }} !</h2>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+      user: {
+        email: "",
+        name: ""
+      }
+    };
   },
   created() {
     if (!localStorage.getItem("token")) this.$router.push("/login");
   },
+  async mounted() {
+    try {
+      const response = await axios.get("api/users/getuser", {
+        headers: { token: localStorage.getItem("token") }
+      });
+      console.log(response);
+      this.user.email = response.data.email;
+      this.user.name = response.data.name;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 </script>
--- server/routes/api/Users.js
+++ server/routes/api/Users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../../models/User");
+const JWT = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -13,15 +14,30 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.json(user);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     res.json(user);
+//   } catch (err) {
+//     res.json({ message: err });
+//   }
+// });
 
+router.get("/getuser", async (req, res) => {
+  const token = req.headers.token;
+  JWT.verify(token, "secretkey", async (err, decoded) => {
+    if (err)
+      return res.json(401).json({ title: "Unauthorized user!", error: err });
+
+    try {
+      const user = await User.findById(decoded.userID);
+
+      return res.status(200).json({ email: user.email, name: user.name });
+    } catch (err) {
+      return res.status(401).json({ title: "Unauthorized user!", error: err });
+    }
+  });
+});
 router.post("/", async (req, res) => {
   const user = new User({
     email: req.body.email,
