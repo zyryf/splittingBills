2e9a599d46bf775c015e65bf6cf62d50ce6758f6
Jakub Markowski <gukeskl@gmail.com>
2020-08-26 15:57:12 +0200

    login modify

--- client/src/views/LogIn.vue
+++ client/src/views/LogIn.vue
@@ -1,35 +1,10 @@
 <template>
-  <div class="form-wrapper">
-    <h2>Here you can <strong>log in </strong></h2>
-    <b-form @submit.prevent="submitForm">
-      <b-form-group id="input-group-1" label="Your Email:" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="user.email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="user.password"
-          required
-          placeholder="Enter password"
-          type="password"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Log in</b-button>
-
-      <b-button class="ml-4 fb-color" @click="logInFB"
-        >Log in with Facebook</b-button
-      >
-    </b-form>
-
-    <b-alert v-if="error" show variant="danger" class="my-2">{{
-      error
-    }}</b-alert>
+  <div id="login ">
+    <h1>Here you can Log in</h1>
+    <p>Enter your email</p>
+    <v-text-field label="E-mail" outlined></v-text-field>
+    <p>Enter your password</p>
+    <v-text-field label="Password" color="primary" background-color="" outlined></v-text-field>
   </div>
 </template>
 
@@ -92,25 +67,15 @@ export default {
 };
 </script>
 
-<style scoped>
-h2 {
-  text-align: center;
+<style lang="scss" scoped>
+#ligin {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  padding: 30px;
 }
 
-h2 strong {
-  color: #42b983;
-}
-.form-wrapper {
-  width: 40%;
-  margin: 50px auto;
-  text-align: left;
-}
-
-.fb-color {
-  background-color: #3b5998;
-  border: none;
-}
-.fb-color:hover {
-  background-color: #527bd3;
+h1 {
 }
 </style>
