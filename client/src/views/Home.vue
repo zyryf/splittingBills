9781e8a53da318b0c8b5d595dc3e6f38a271fa44
Jakub Markowski <gukeskl@gmail.com>
2020-08-26 15:12:05 +0200

    gitignore repair

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,10 +2,10 @@
   <div class="home">
     <h1 class="header">Share your expenses in a comfortable way</h1>
     <p>Try our easy to use application for splitting the bills</p>
-    <h5>Create an account or log in</h5>
     <div class="buttons">
-      <v-btn  rounded color="primary"  class="m-2">Sign Up</v-btn>
-      <v-btn rounded outlined color="primary" class="m-2">Log In</v-btn>
+      <h5>Create an account or log in</h5>
+      <v-btn  rounded color="primary" block class="m-2 ">Sign Up</v-btn>
+      <v-btn rounded outlined block color="primary" class="m-2 ">Log In</v-btn>
     </div>
     <p>Want to learn more? Check the <strong>about</strong> section</p>
   </div>
@@ -51,8 +51,11 @@ export default {
   flex-direction: column;
   align-items: center;
 
-  width: 100%;
+  width: 80%;
 }
 
+.btn {
+  width: 80%;
+}
 
 </style>
