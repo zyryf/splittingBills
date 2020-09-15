411e70e80bfbc5bc2813a3f64db87ed61f50a7f5
Jakub Markowski <gukeskl@gmail.com>
2020-09-15 23:51:52 +0200

    - add CHANGELOG.md - change docker images name

--- release.sh
+++ release.sh
@@ -6,10 +6,14 @@ export NODE_ENV="production"
 
 npm run build --prefix modules/client
 
-docker build -t splittingbills/client:$VERSION_TAG ./modules/client || exit 1
-docker build -t splittingbills/server:$VERSION_TAG ./modules/server || exit 1
-docker build -t splittingbills/nginx:$VERSION_TAG ./modules/nginx || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/client:$VERSION_TAG ./modules/client || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/server:$VERSION_TAG ./modules/server || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/nginx:$VERSION_TAG ./modules/nginx || exit 1
 
 # Push images to GitHub registry
+
+docker push docker.pkg.github.com/zyryf/splittingbills/client:$VERSION_TAG
+docker push docker.pkg.github.com/zyryf/splittingbills/server:$VERSION_TAG
+docker push docker.pkg.github.com/zyryf/splittingbills/nginx:$VERSION_TAG
 
 echo $VERSION_TAG
