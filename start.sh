749530d866221a2acb32e89f90e7a36e36a855e7
Jakub Markowski <gukeskl@gmail.com>
2020-09-15 23:51:52 +0200

    - add CHANGELOG.md - change docker images name

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ docker-compose -f deployment/docker-compose.dev.yml rm || exit 1
 
 # Build client docker image
 
-docker build -t splittingbills/client ./modules/client || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/client ./modules/client || exit 1
 
 # Build server docker image
 
-docker build -t splittingbills/server ./modules/server || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/server ./modules/server || exit 1
 
 # Build nginx docker image
 
-docker build -t splittingbills/nginx ./modules/nginx || exit 1
+docker build -t docker.pkg.github.com/zyryf/splittingbills/nginx ./modules/nginx || exit 1
 
 # Start app
 
